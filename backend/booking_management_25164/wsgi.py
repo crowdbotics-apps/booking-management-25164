0350be6bc5ffddb94f62d8693363106670293889
Crowdbotics <team@crowdbotics.com>
2021-03-21 15:25:32 +0000

    Update models

--- backend/booking_management_25164/wsgi.py
+++ backend/booking_management_25164/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'booking_management_25164.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "booking_management_25164.settings")
 
 application = get_wsgi_application()
